27664361900f1acd6a8cf0e900d0841198120605
Maksim Kostromin <daggerok@gmail.com>
2019-05-09 01:16:49 +0300

    Add trace test.

--- composing-software/test/chainingMeansComposingSpec.js
+++ composing-software/test/chainingMeansComposingSpec.js
@@ -10,4 +10,19 @@ describe("if you are chaining, you are composing", () => {
     console.log(result);
     expect(result).toBeDefined();
   });
+
+  it('should trace', () => {
+    const trace = label => value => {
+      console.log(`${label}: ${value}`);
+      return value;
+    };
+    const g = n => n + 1;
+    const f = n => n * 2;
+    const doAction = value => {
+      const beforeG = trace('before g')(value);
+      const afterG = trace('after g')(g(beforeG));
+      return trace('after f')(f(afterG));
+    };
+    expect(doAction(20)).toBe(42);
+  });
 });
