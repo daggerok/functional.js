81216a72803e8cc4c2c843b1581cc95540baa3a0
Maksim Kostromin <daggerok@gmail.com>
2019-05-09 22:02:33 +0300

    Add cypress.

---
+++ cypress-testing/README.md
@@ -0,0 +1,36 @@
+# cypress-testing
+
+_install cypress_
+
+```bash
+npm install cypress -ED
+```
+
+_open it_
+
+```bash
+./node_modules/.bin/cypress install
+./node_modules/.bin/cypress open
+# or $(npm bin)/cypress open
+# or npx cypress open
+```
+
+_added npm scripts:_
+
+```bash
+npm run cypress-install
+npm run cypress-open
+```
+
+_press 'OK, got it!'_
+
+_cleanup_
+
+```bash
+$(npm bin)/cypress cache path
+$(npm bin)/cypress cache clear
+```
+
+_links_
+
+* [Testing Your App](https://docs.cypress.io/guides/getting-started/testing-your-app.html#Step-1-Start-your-server)
---
+++ cypress-testing/cypress/integration/hello_spec.js
@@ -0,0 +1,21 @@
+describe('hello', () => { // mocha: describe
+  it('should say hello and test that true is true', () => { // mocha: it
+    console.log('hello!');
+    expect(true).to.equal(true); // chai: expect
+  });
+
+  it('should test google search', () => {
+    // given:
+    cy.visit('https://google.com');
+    // when:
+    cy.contains('Google');
+    // and
+    cy.get('input[name="q"]')
+      .type('cypress is awesome!')
+      .type('{enter}');
+    // then
+    cy.get('div#search div.srg div.g')
+      .should('be.visible')
+      .should('have.length', 9);
+  });
+});
