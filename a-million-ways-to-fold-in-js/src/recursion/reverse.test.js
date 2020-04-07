8578d7630741192c70e149adfa8f280258d7b676
Maksim Kostromin <daggerok@gmail.com>
2020-04-08 00:52:57 +0300

    Trust me, it's not badly written. It's just way above your head. (C) whatthecommit.com

--- a-million-ways-to-fold-in-js/src/recursion/reverse.test.js
+++ a-million-ways-to-fold-in-js/src/recursion/reverse.test.js
@@ -1,10 +1,32 @@
 import test from 'ava';
-import { first, rest } from '../dry';
+import { first, rest } from '../dry.js';
 
-const reverse = xs => {
+const reverseIncorrectly = xs => {
   if (xs.length === 0) return []; // base case
-  return reverse(rest(xs)/* <- recursion */).concat(first(xs)); // action
+  return reverseIncorrectly(rest(xs)/* <- recursion */).concat(first(xs)); // action
 }
+
+test('should incorrectly reverse empty array', t => {
+  t.deepEqual(reverseIncorrectly([]), []); // but:
+  t.not(reverseIncorrectly([]), []);
+});
+
+test('should incorrectly reverse non-empty array', t => {
+  t.deepEqual(reverseIncorrectly([1, 2, 3]), [3, 2, 1]); // but:
+  t.not(reverseIncorrectly([1, 2, 3]), [3, 2, 1]);
+});
+
+/* correct */
+
+const reverse = list => {
+  const _reduce = (f, acc, xs) => {
+    // console.info('acc:', acc, 'xs:', xs, '!xs:', !xs, 'xs.length:', xs.length, 'xs.length === 0:', xs.length === 0)
+    if (xs.length === 0) return acc;
+    // console.info('first(xs):', first(xs), 'rest(xs):', rest(xs))
+    return _reduce(f, f(acc, first(xs)), rest(xs));
+  };
+  return _reduce((acc, x) => [x].concat(acc), [], list);
+};
 
 test('should reverse empty array', t => {
   t.deepEqual(reverse([]), []); // but:
--- a-million-ways-to-fold-in-js/src/recursion/sum.test.js
+++ a-million-ways-to-fold-in-js/src/recursion/sum.test.js
@@ -1,13 +1,34 @@
 import test from 'ava';
-import { first, rest } from '../dry';
+import { first, rest } from '../dry.js';
 
-const sum = xs => {
+const sumIncorrectly = xs => {
+  // console.info('calling sumIncorrectly for argument', xs);
   if (xs.length === 0) return 0; // base case
-  return first(xs)/* action */+ sum(rest(xs)); // <- recursion
+  return first(xs)/* action */+ sumIncorrectly(rest(xs)); // <- recursion
 }
 
+test('should incorrectly sum empty array', t => {
+  t.is(sumIncorrectly([]), 0, 'oops, expected: 0');
+});
+
+test('should incorrectly sum non-empty array', t => {
+  t.is(sumIncorrectly([1, 2, 3]), 6);
+});
+
+/* correct */
+
+const sum = list => {
+  // console.info('calling sum for argument', list);
+  const _sum = (acc, xs) => {
+    // console.info('calling _sum for arguments', acc, xs);
+    if (xs.length === 0) return acc;
+    return _sum(acc + first(xs), rest(xs));
+  };
+  return _sum(0, list);
+};
+
 test('should sum empty array', t => {
-  t.is(sum([]), 0, 'oops, expected: 0');
+  t.is(sum([]), 0);
 });
 
 test('should sum non-empty array', t => {
